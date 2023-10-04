66d4e61cf655b984b6f06f4ed534b547cef5a6e3
chenjianbo <specialwave@foxmail.com>
2023-10-04 20:40:44 +0800

    fix: cols potentially not a natural number, this will cause column overflow

--- masonry.js
+++ masonry.js
@@ -1,10 +1,10 @@
 /*!
- * Masonry v4.2.2-hotfix.1
+ * Masonry v4.2.2-hotfix.6
  * Cascading grid layout library
  * https://masonry.desandro.com
  * MIT License
  * original by David DeSandro
- * hotfix by Paper Folding
+ * patch by Paper Folding
  */
 
 ( function( window, factory ) {
@@ -76,16 +76,16 @@
 
     // calculate columns
     var containerWidth = this.containerWidth + this.gutter;
-    var cols = containerWidth / columnWidth;
-    if (this.options.columns) {  // if `columns` option is explicitly delivered, use that cuz columns calculated by masonry is not accurately guaranteed
+    var cols;
+    if (this.options.columns)  // if `columns` option is explicitly delivered, use that cuz columns calculated by masonry is not accurately guaranteed
       cols = this.options.columns;
-    } else {
-      // fix rounding errors, typically with gutters
-      var excess = columnWidth - containerWidth % columnWidth;
-      // if overshoot is less than a pixel, round up, otherwise floor it
-      var mathMethod = excess && excess < 1 ? 'round' : 'floor';
-      cols = Math[mathMethod](cols);
-    }
+    else
+      cols = containerWidth / columnWidth;
+    // fix rounding errors, typically with gutters
+    var excess = columnWidth - containerWidth % columnWidth;
+    // if overshoot is less than a pixel, round up, otherwise floor it
+    var mathMethod = excess && excess < 1 ? 'round' : 'floor';
+    cols = Math[mathMethod](cols);
     this.cols = Math.max( cols, 1 );
     // it looks like masonry will incorrectly calculate actual columns
     // when each items' width are fixed percentage value,
